e05471432dc334ea01767bec264a6032ca7168d3
Sergei Turukin <rampage644@gmail.com>
2015-02-25 14:11:08 +0300

    Parse command line, enable blueflood flusher by default

--- twisted/plugins/graphite_blueflood_plugin.py
+++ twisted/plugins/graphite_blueflood_plugin.py
@@ -1,5 +1,3 @@
-from ConfigParser import ConfigParser
-
 from twisted.application.service import IServiceMaker, Service
 from twisted.internet.endpoints import serverFromString
 from twisted.internet.protocol import Factory
@@ -10,11 +8,21 @@ from twisted.python import usage, log
 from zope.interface import implementer
 
 from bluefloodserver.protocols import MetricLineReceiver
-from bluefloodserver.collect import MetricCollection, ConsumeFlush
+from bluefloodserver.collect import MetricCollection, ConsumeFlush, BluefloodFlush
+from bluefloodserver.blueflood import BluefloodEndpoint
 
 
 class Options(usage.Options):
-    optParameters = []
+    DEFAULT_TTL = 60 * 60 * 24
+    optParameters = [
+        ['endpoint', 'e', 'tcp:2003', 'Twisted formatted endpoint to listen to plain text protocol metrics'],
+        ['interval', 'i', 30, 'Metric send interval, sec'],
+        ['blueflood', 'b', 'http://localhost:19000', 'Blueflood server ingest URL (schema, host, port)'],
+        ['tenant', 't', '', 'Blueflood tenant ID'],
+        ['ttl', '', DEFAULT_TTL, 'TTL value for metrics, sec'],
+        ['user', 'u', '', 'Rackspace authentication username. Leave empty if no auth is required'],
+        ['pass', 'p', '', 'Rackspace authentication password']
+    ]
 
 
 class GraphiteMetricFactory(Factory):
@@ -27,20 +35,27 @@ class GraphiteMetricFactory(Factory):
         self._metric_collection.collect(metric, datapoint)
 
     def flushMetric(self):
-        self._metric_collection.flush()
+        try:
+            self._metric_collection.flush()
+        except Exception, e:
+            log.err(e)
 
 
 class MetricService(Service):
 
-    def __init__(self, endpoint, interval):
+    def __init__(self, endpoint, interval, blueflood_url, tenant, ttl):
         self._endpoint = endpoint
         self.flush_interval = interval
+        self.blueflood_url = blueflood_url
+        self.tenant = tenant
+        self.ttl = ttl
 
     def startService(self):
         from twisted.internet import reactor
 
         server = serverFromString(reactor, self._endpoint)
         factory = GraphiteMetricFactory.forProtocol(MetricLineReceiver)
+        self._setup_blueflood(factory)
         self.timer = LoopingCall(factory.flushMetric)
         self.timer.start(self.flush_interval)
 
@@ -49,6 +64,13 @@ class MetricService(Service):
     def stopService(self):
         self.timer.stop()
 
+    def _setup_blueflood(self, factory):
+        client = BluefloodEndpoint(
+            ingest_url=self.blueflood_url,
+            tenant=self.tenant)
+        flusher = BluefloodFlush(client=client, ttl=self.ttl)
+        factory._metric_collection.flusher = flusher
+
 @implementer(IServiceMaker, IPlugin)
 class MetricServiceMaker(object):
     tapname = 'blueflood-forward'
@@ -56,11 +78,12 @@ class MetricServiceMaker(object):
     options = Options
 
     def makeService(self, options):
-        config = ConfigParser()
-
         return MetricService(
-            endpoint='tcp:2003',
-            interval=5
+            endpoint=options['endpoint'],
+            interval=float(options['interval']),
+            blueflood_url=options['blueflood'],
+            tenant=options['tenant'],
+            ttl=int(options['ttl'])
         )
 
 

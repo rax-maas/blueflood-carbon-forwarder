8b0350bfa1b430e5ab0d0476378f4bb157fcc034
Sergei Turukin <rampage644@gmail.com>
2015-03-02 18:56:51 +0300

    WIP migrate blueflood client from urllib2 to twisted

--- tests/test_blueflood.py
+++ tests/test_blueflood.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import urllib2
 import time
-
-from bluefloodserver.blueflood import BluefloodEndpoint
+import json 
 import pytest
+
+from twisted.web.client import Agent, FileBodyProducer, readBody
+from bluefloodserver.blueflood import BluefloodEndpoint
 
 
 try:
@@ -19,14 +21,17 @@ else:
 def setup():
     return BluefloodEndpoint()
 
+@pytest.inlineCallbacks
 @pytest.mark.skipif(skip, reason="Blueflood isn't running")
 def testSingleIngest(setup):
     endpoint = setup
     name = 'example.metric.single.ingest'
     ttl = 10
     endpoint.ingest(name, 1376509892612, 50, ttl) 
-    assert endpoint.commit() == ''
+    d = yield endpoint.commit()
+    assert d.code == 200
 
+@pytest.inlineCallbacks
 @pytest.mark.skipif(skip, reason="Blueflood isn't running")
 def testListIngest(setup):
     endpoint = setup
@@ -36,13 +41,15 @@ def testListIngest(setup):
                     [1376509892612, 1376509892613, 1376509892614], 
                     [50, 51, 52], 
                     ttl)
-    assert endpoint.commit() == ''
+    d = yield endpoint.commit()
+    assert d.code == 200
     with pytest.raises(Exception):
         endpoint.ingest(name, 
                         [1376509892612, 1376509892613, 1376509892614], 
                         50, 
                         ttl)
 
+@pytest.inlineCallbacks
 @pytest.mark.skipif(skip, reason="Blueflood isn't running")
 def testMultipleIngest(setup):
     endpoint = setup
@@ -51,12 +58,15 @@ def testMultipleIngest(setup):
     times, values = list(range(5)), [50.0] * 5
     for t, v in zip(times, values):
          endpoint.ingest(name, t, v, ttl)
-    assert endpoint.commit() == ''
-    data = endpoint.retrieve(name, 0, 10, 200)
-    assert len(data['values']) == 5
+    d = yield endpoint.commit()
+    assert d.code == 200
+    resp = yield endpoint.retrieve_resolution(name, 0, 10)
+    data = yield readBody(resp)
+    assert len(json.loads(data)['values']) == 5
 
+@pytest.inlineCallbacks
 @pytest.mark.skipif(skip, reason="Blueflood isn't running")
-def testRetrieve(setup):
+def testRetrieveByResolution(setup):
     endpoint = setup
     name = 'example.metric.retrieve'
     ttl = 10
@@ -64,9 +74,33 @@ def testRetrieve(setup):
     value = 50
     # insert first
     endpoint.ingest(name, timestamp, value, ttl)
-    assert endpoint.commit() == ''
+    d = yield endpoint.commit()
+    assert d.code == 200
     # range is 0-time
-    data = endpoint.retrieve_resolution(name, 0, timestamp + 10)
+    resp = yield endpoint.retrieve_resolution(name, 0, timestamp + 10)
+    rdata = yield readBody(resp)
+    data = json.loads(rdata)
+    assert len(data) != 0
+    assert len(data['values']) != 0
+    assert data['values'][0]['numPoints'] == 1
+    assert data['values'][0]['average'] == value
+
+@pytest.inlineCallbacks
+@pytest.mark.skipif(skip, reason="Blueflood isn't running")
+def testRetrieveByPoints(setup):
+    endpoint = setup
+    name = 'example.metric.retrieve'
+    ttl = 10
+    timestamp = int(time.time())
+    value = 50
+    # insert first
+    endpoint.ingest(name, timestamp, value, ttl)
+    d = yield endpoint.commit()
+    assert d.code == 200
+    # range is 0-time
+    resp = yield endpoint.retrieve_points(name, 0, timestamp + 10, 200)
+    rdata = yield readBody(resp)
+    data = json.loads(rdata)
     assert len(data) != 0
     assert len(data['values']) != 0
     assert data['values'][0]['numPoints'] == 1

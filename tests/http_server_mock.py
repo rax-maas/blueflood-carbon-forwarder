1cd2988e524f9abcacab857e0eddc365e8793c6d
Sergei Turukin <rampage644@gmail.com>
2015-03-10 12:42:07 +0300

    Add integration tests

---
+++ tests/http_server_mock.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import time
+from twisted.web import resource, server
+from twisted.internet import reactor, endpoints
+
+
+class MockServerHandler(resource.Resource):
+    isLeaf = True
+
+    def __init__(self, cv=None):
+        resource.Resource.__init__(self)
+        self.cv = cv
+        self.data = []
+        self.reply = []
+        self.default_reply = (200, '', {})
+
+    def should_reply_once(self, status, response, headers):
+        self.reply.append((status, response, headers))
+
+    def should_reply_forever(self, status, response, headers):
+        self.default_reply = (status, response, headers)
+
+    def reset_to_default_reply(self):
+        self.reply = []
+
+    def _get_next_reply(self):
+        code, response, headers = self.reply[0] if self.reply else self.default_reply
+        try:
+            del self.reply[0]
+        except IndexError:
+            pass
+        return code, response, headers
+
+
+    def render_POST(self, request):
+        data = request.content.read()
+        self.data += [(time.time(), dict(request.received_headers), request.path, data)]
+        print self.data[-1]
+        print ''
+        if self.cv:
+            with self.cv:
+                self.cv.notify()
+
+        code, response, headers = self._get_next_reply()
+        request.setResponseCode(code)
+        for key, value in headers.items():
+            request.setHeader(key, value)
+        return response
+
+if __name__ == '__main__':
+    b_handler = MockServerHandler()
+    a_handler = MockServerHandler()
+    response = """{"access":{"token":{"id":"eb5e1d9287054898a55439137ea68675","expires":"2014-12-14T22:54:49.574Z","tenant":{"id":"836986","name":"836986"}}}}"""
+    endpoints.serverFromString(reactor, "tcp:8000").listen(server.Site(b_handler))
+    endpoints.serverFromString(reactor, "tcp:8001").listen(server.Site(a_handler))
+    a_handler.should_reply_forever(200, response, {})
+    reactor.run()
---
+++ tests/test_integration.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+import os
+import json
+import pickle
+import pytest
+import SocketServer
+import socket
+import struct
+import subprocess
+import threading
+import time
+import urllib2
+
+import http_server_mock
+
+from twisted.internet import reactor, endpoints
+from twisted.web import server
+
+pytestmark = pytest.mark.integration
+
+
+b_handler = None
+a_handler = None
+t = None
+
+
+TENANT = 'tenant'
+AUTH_URL = 'http://localhost:8001'
+AUTH_USER = 'user1'
+AUTH_KEY = 'key2'
+BLUEFLOOD_URL = 'http://localhost:8000'
+LISTEN_URL = ('localhost', 2004)
+INTERVAL = 5
+
+def run_twisted(user=None, key=None):
+    args = ['twistd', '-n', '-l', 'log.txt', 'blueflood-forward',
+     '-b', BLUEFLOOD_URL, '-i', str(INTERVAL), '-t', TENANT]
+    if user:
+        args.extend(['-u', user, '-k', key, '--auth_url', AUTH_URL])
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return p
+
+def send_to_socket(listOfMetricTuples):
+    payload = pickle.dumps(listOfMetricTuples, protocol=2)
+    header = struct.pack("!L", len(payload))
+    message = header + payload
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(LISTEN_URL)
+    sock.sendall(message)
+    sock.close()
+
+def setup_module(module):
+    """ setup any state specific to the execution of the given module."""
+    
+    global b_handler, a_handler, t
+    # blueflood server mock
+    b_handler = http_server_mock.MockServerHandler()
+    # auth server mock
+    a_handler = http_server_mock.MockServerHandler()
+
+    endpoints.serverFromString(reactor, "tcp:8000").listen(server.Site(b_handler))
+    endpoints.serverFromString(reactor, "tcp:8001").listen(server.Site(a_handler))
+
+    t = threading.Thread(target=reactor.run, args=(0, ))
+    t.daemon = True
+    t.start()
+
+def teardown_module(module):
+    """ teardown any state that was previously setup with a setup_module
+    method.
+    """
+    global t
+
+    reactor.stop()
+    t = None
+
+
+@pytest.fixture
+def twistd(request):
+    return fixture_run_twisted(request, None, None)
+
+@pytest.fixture
+def twistd_auth(request):
+    return fixture_run_twisted(request, AUTH_USER, AUTH_KEY)
+    
+def fixture_run_twisted(request, user, key):
+    p = run_twisted(user, key)
+    def fin():
+        p.terminate()
+        print 'Waiting process to terminate'
+        watchdog_timer = threading.Timer(5.0, p.kill, [p])
+        watchdog_timer.start()
+        p.wait()
+        watchdog_timer.cancel()
+        assert p.returncode == 0
+    request.addfinalizer(fin)
+    return p
+
+def setup_and_teardown_handler(request, handler):
+    cv = threading.Condition()
+    handler.cv = cv
+    def fin():
+        handler.reset_to_default_reply()
+        handler.cv = None
+        handler.data = []
+    request.addfinalizer(fin)
+    return handler
+
+@pytest.fixture
+def blueflood_handler(request):
+    global b_handler
+    return setup_and_teardown_handler(request, b_handler)
+
+@pytest.fixture
+def auth_handler(request):
+    global a_handler
+    return setup_and_teardown_handler(request, a_handler)
+
+def test_noauth(blueflood_handler, twistd):
+    blueflood_handler.should_reply_forever(200, '', {})
+
+    time.sleep(1.5)
+    cv = blueflood_handler.cv
+    timestamp = int(time.time())
+    value = 10.0
+    name = 'foo.bar.baz'
+    send_to_socket([(name, (timestamp, value))])
+    with cv:
+        cv.wait(INTERVAL * 2)
+
+    assert len(blueflood_handler.data) == 1
+    _, headers, path, data = blueflood_handler.data[0]
+    assert path == '/v2.0/tenant/ingest'
+    metrics = json.loads(data)
+    assert len(metrics) == 1
+    assert metrics[0]['metricValue'] == value
+    assert metrics[0]['collectionTime'] == timestamp
+    assert metrics[0]['metricName'] == name
+
+def test_auth(blueflood_handler, auth_handler, twistd_auth):
+    response = """{"access":{"token":{"id":"eb5e1d9287054898a55439137ea68675","expires":"2014-12-14T22:54:49.574Z","tenant":{"id":"836986","name":"836986"}}}}"""
+
+    auth_handler.should_reply_forever(200, response, {})
+    blueflood_handler.should_reply_once(200, '', {})
+    time.sleep(1.0)
+
+    timestamp = int(time.time())
+    value = 10.0
+    name = 'foo.bar.baz'
+    send_to_socket([(name, (timestamp, value))])
+
+    with auth_handler.cv:
+        auth_handler.cv.wait(INTERVAL)
+    assert len(auth_handler.data) == 1
+
+    with blueflood_handler.cv:
+        blueflood_handler.cv.wait(INTERVAL)
+    assert len(blueflood_handler.data) == 1
+    headers = blueflood_handler.data[0][1]
+    assert 'x-auth-token' in headers
+    assert headers['x-auth-token'] == 'eb5e1d9287054898a55439137ea68675'

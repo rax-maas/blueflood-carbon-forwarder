3a797020c26db9f3d46d14312308ea2cbb1a6d36
Gaurav Bajaj <goru97@users.noreply.github.com>
2016-03-07 19:37:49 -0800

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,4 +9,9 @@
 : ${KEYSTONE_USER=""}
 : ${KEYSTONE_KEY=""}
 : ${AUTH_URL="https://identity.api.rackspacecloud.com/v2.0/tokens"}
-twistd --pidfile /var/run/twistd.pid --nodaemon --logger carbonforwarderlogging.forwarder_log_observer.get_log_observer blueflood-forward -e $ENDPOINT -b $BLUEFLOOD_URL -u $KEYSTONE_USER -k $KEYSTONE_KEY --auth_url $AUTH_URL --tenant $TENANT_ID -p $METRIC_PREFIX --ttl $DEFAULT_TTL --limit=$LIMIT 
+if [[ -z "$METRIC_PREFIX" ]]
+then
+twistd --pidfile /var/run/twistd.pid --nodaemon --logger carbonforwarderlogging.forwarder_log_observer.get_log_observer blueflood-forward -e $ENDPOINT -b $BLUEFLOOD_URL -u $KEYSTONE_USER -k $KEYSTONE_KEY --auth_url $AUTH_URL --tenant $TENANT_ID --ttl $DEFAULT_TTL --limit=$LIMIT 
+else
+twistd --pidfile /var/run/twistd.pid --nodaemon --logger carbonforwarderlogging.forwarder_log_observer.get_log_observer blueflood-forward -e $ENDPOINT -b $BLUEFLOOD_URL -u $KEYSTONE_USER -k $KEYSTONE_KEY --auth_url $AUTH_URL --tenant $TENANT_ID -p $METRIC_PREFIX --ttl $DEFAULT_TTL --limit=$LIMIT
+fi

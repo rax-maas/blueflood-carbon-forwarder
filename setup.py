58bc6501ca7827cf53829e9d385e46c8b50b68f2
Richard Sartor <richard.sartor@rackspace.com>
2016-06-07 16:57:09 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PyTest(TestCommand):
 
 setup(
     name='blueflood-carbon-forwarder',
-    version="0.3.2",
+    version="0.4.0",
     url='https://github.com/rackerlabs/blueflood-carbon-forwarder',
     license='Apache Software License',
     author='Rackspace Metrics',
